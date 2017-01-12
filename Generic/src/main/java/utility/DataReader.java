b94ab47229503abecb7bc27573936be14b7564fb
bwaheed <bilalwaheedch@me.com>
2017-01-12 16:04:21 -0500

    Fix Conflicts

--- Generic/src/main/java/utility/DataReader.java
+++ Generic/src/main/java/utility/DataReader.java
@@ -1,21 +1,16 @@
 package utility;
 
-/**
- * Created by izran on 1/2/2017.
- */
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
-
-        import org.apache.poi.hssf.usermodel.HSSFCell;
-        import org.apache.poi.hssf.usermodel.HSSFRow;
-        import org.apache.poi.hssf.usermodel.HSSFSheet;
-        import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-        import org.apache.poi.ss.usermodel.Cell;
-        import org.apache.poi.ss.usermodel.Row;
-
-        import java.io.File;
-        import java.io.FileInputStream;
-        import java.io.FileOutputStream;
-        import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 
 public class DataReader {
@@ -25,53 +20,52 @@ public class DataReader {
     FileOutputStream fio = null;
     int numberOfRows, numberOfCol, rowNum;
 
-    public String[][] fileReader1(String path) throws IOException {
-        String[][] data = {};
+    public String[][] fileReader(String path)throws IOException{
+        String [] [] data = {};
         File file = new File(path);
         FileInputStream fis = new FileInputStream(file);
         wb = new HSSFWorkbook(fis);
         sheet = wb.getSheetAt(0);
         numberOfRows = sheet.getLastRowNum();
-        numberOfCol = sheet.getRow(0).getLastCellNum();
-        data = new String[numberOfRows + 1][numberOfCol + 1];
+        numberOfCol =  sheet.getRow(0).getLastCellNum();
+        data = new String[numberOfRows+1][numberOfCol+1];
 
-        for (int i = 0; i < data.length; i++) {
+        for(int i=1; i<data.length; i++){
             HSSFRow rows = sheet.getRow(i);
-            for (int j = 0; j < numberOfCol; j++) {
+            for(int j=0; j<numberOfCol; j++){
                 HSSFCell cell = rows.getCell(j);
                 String cellData = getCellValue(cell);
                 data[i][j] = cellData;
             }
         }
-        return data;
+        return  data;
     }
-
-    public String[] fileReader(String path) throws IOException {
-        String[] data = {};
+    public String[] colReader(String path, int col)throws IOException{
+        String []  data = {};
         File file = new File(path);
         FileInputStream fis = new FileInputStream(file);
         wb = new HSSFWorkbook(fis);
         sheet = wb.getSheetAt(0);
         numberOfRows = sheet.getLastRowNum();
-        numberOfCol = sheet.getRow(0).getLastCellNum();
-        data = new String[numberOfRows + 1];
+        numberOfCol =  col;
+        data = new String[numberOfRows];
 
-        for (int i = 1; i < data.length; i++) {
-            HSSFRow rows = sheet.getRow(i);
-            for (int j = 0; j < numberOfCol; j++) {
+        for(int i=0; i<data.length; i++){
+            HSSFRow rows = sheet.getRow(i+1);
+            for(int j=0; j<numberOfCol; j++){
                 HSSFCell cell = rows.getCell(j);
                 String cellData = getCellValue(cell);
                 data[i] = cellData;
             }
         }
-        return data;
+        return  data;
     }
 
-    public String getCellValue(HSSFCell cell) {
+    public String getCellValue(HSSFCell cell){
         Object value = null;
 
         int dataType = cell.getCellType();
-        switch (dataType) {
+        switch(dataType){
             case HSSFCell.CELL_TYPE_NUMERIC:
                 value = cell.getNumericCellValue();
                 break;
@@ -86,7 +80,7 @@ public class DataReader {
 
     }
 
-    public void writeBack(String value) throws IOException {
+    public void writeBack(String value)throws IOException {
         wb = new HSSFWorkbook();
         sheet = wb.createSheet();
         Row row = sheet.createRow(rowNum);
@@ -94,7 +88,7 @@ public class DataReader {
 
         fio = new FileOutputStream(new File("ExcelFile.xls"));
         wb.write(fio);
-        for (int i = 0; i < row.getLastCellNum(); i++) {
+        for(int i=0; i<row.getLastCellNum(); i++){
             row.createCell(i);
             cell.setCellValue(value);
         }
@@ -102,3 +96,4 @@ public class DataReader {
         wb.close();
     }
 }
+
