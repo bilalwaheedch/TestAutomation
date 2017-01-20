651f542846c0032d379c420b1fb9ee756799477c
bwaheed <bilalwaheedch@me.com>
2017-01-19 20:25:26 -0500

    Facebook Framework Update

--- Facebook/src/main/java/CreatePage/UiCreatePage.java
+++ Facebook/src/main/java/CreatePage/UiCreatePage.java
@@ -1,13 +1,16 @@
 package CreatePage;
 
+import Homepage.UiYourAccountMenuTab;
+import methods.CommonMethods;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * Created by Bilal on 19-01-2017.
  */
-public class UiCreatePage {
+public class UiCreatePage extends CommonMethods{
     @FindBy(how = How.XPATH,using = "html/body/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/div[1]/div[1]/div/div[2]/h2")
     public WebElement pageHeading;
     public String pageHeadingText = "Create a Page";
