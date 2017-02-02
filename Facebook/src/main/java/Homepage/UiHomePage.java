0ff8d0ce2ee04728617dbc49cfab315581f85013
bwaheed <bilalwaheedch@me.com>
2017-02-02 13:48:33 -0500

    Facebook Post Status Test Added

--- Facebook/src/main/java/Homepage/UiHomePage.java
+++ Facebook/src/main/java/Homepage/UiHomePage.java
@@ -1,7 +1,9 @@
 package Homepage;
 
 import methods.CommonMethods;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
@@ -14,4 +16,16 @@ public class UiHomePage extends CommonMethods {
 
     @FindBy(how = How.XPATH,using = ".//*[@id='u_g_0']")
     public WebElement headingCreateGroup;
+
+    @FindBy(name = "xhpc_message")
+    public WebElement barPostStatus;
+
+    @FindBy(className = "_1mf _1mj")
+    public WebElement tfPostStatus;
+
+    public void typePostStatusBar(String text, WebDriver driver){
+        barPostStatus.click();
+        new Actions(driver).sendKeys(text).build().perform();
+//        tfPostStatus.sendKeys(text);
+    }
 }
---
+++ Facebook/src/test/java/TestHomepage/TestPostStatus.java
@@ -0,0 +1,26 @@
+package TestHomepage;
+
+import Homepage.UiHomePage;
+import methods.CommonMethods;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import utility.DriverFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by Bilal on 02-02-2017.
+ */
+public class TestPostStatus extends CommonMethods{
+    public UiHomePage uiHomePage() throws IOException {
+        WebDriver driver = DriverFactory.getInstance().getDriver();
+        signIn(driver);
+        return PageFactory.initElements(driver,UiHomePage.class);
+    }
+    @Test
+    public void testPostStatus() throws IOException, InterruptedException {
+        uiHomePage().typePostStatusBar("Test Test Test",DriverFactory.getInstance().getDriver());
+        sleepFor(3);
+    }
+}
